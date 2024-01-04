ad8bd2e984b6635bf0f27e8a10cae60f662b3d1c
Rafael Santos <rafaeldsantos442@gmail.com>
2024-01-04 11:59:32 -0300

    Realizado a criação do Header com tailwind

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,23 +1,37 @@
-import React from 'react'
+import React from 'react';
 
 function Header() {
   return (
-    <header>
-        <section>
-            <div className='logo'> 
-                <a href="#">
-                    Logo
-                </a>
-            </div>
-            <ul className='links'>
-                <li>
-                    <a href="#">Home</a>
-                    <a href="#">Serviços</a>
-                </li>
-            </ul>
-        </section>
+    <header className='bg-gray-900 text-white p-5'>
+      <section className='container mx-auto flex items-center justify-between'>
+        <div className=''>
+          <a href="#" className='text-xl font-bold'>
+            Logo
+          </a>
+        </div>
+        <ul className='flex space-x-4'>
+          <li>
+            <a href="#" className='hover:text-gray-300'>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#" className='hover:text-gray-300'>
+              Serviços
+            </a>
+          </li>
+          <li>
+            <a href="#" className='hover:text-gray-300'>
+              Contato
+            </a>
+          </li>
+        </ul>
+        <button className='bg-sky-950 hover:bg-blue-500 text-white px-4 py-2 rounded'>
+          Agendar Online
+        </button>
+      </section>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import PainelAgendamento from "../pages/PainelAgendamento";
+import Header from "../components/Header";
 
 function AppRoutes() {
     return (
         <Router>
             <Routes>
                 <Route path="/agendamentos" element={<PainelAgendamento />} />
+                <Route path="/teste" element={<Header/>} />
             </Routes>
         </Router>
     );
